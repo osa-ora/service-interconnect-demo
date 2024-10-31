4ddeb5fddadefad245041fbd1ea58ecbe3f23eb7
Osama Oransa <osa_ora@yahoo.com>
2024-10-31 12:01:27 +0300

    Update FrontController.java

--- frontend/src/main/java/osa/ora/demo/FrontController.java
+++ frontend/src/main/java/osa/ora/demo/FrontController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/front")
@@ -26,15 +28,22 @@ public class FrontController {
     }
 
 	@GetMapping(path = "/test/{account}")
-    public HashMap testApp(@PathVariable(value = "account") Integer account) {
+    public String testApp(@PathVariable(value = "account") Integer account) {
 		String apiUrl = endPoint+account;
 		System.out.println("End point:"+apiUrl);
-        String response = restTemplate.getForObject(apiUrl, String.class);
-		System.out.println("Get Last Transactions for account from url: "+response);
-        return new HashMap(){{
-            put("Welcome"," guest");
-            put("Response:",response);
-        }};
+        String response = "";
+	ResponseEntity<String> restResponse = restTemplate.getForEntity(apiUrl, String.class); // use getForEntity to get ResponseEntity
+	
+	if (restResponse.getStatusCode() == HttpStatus.OK) {
+	    response = restResponse.getBody();
+	} else {
+	    response = "Error: " + restResponse.getStatusCode() + " " + restResponse.getBody();
+	    // Handle based on status code if needed
+	}
+	System.out.println("Get Current Balance for account from url: " + response);
+
+        return response;
+        
     }
 
 	@GetMapping(path = "/")
